87fa76bb1088d6cc67b84d8999adc199c2faf972
Felix-Okeyo <felixokeyo20@gmail.com>
2023-09-03 12:37:08 +0300

    seed half

--- lib/seed.py
+++ lib/seed.py
@@ -1,4 +1,6 @@
 
+from faker import Faker
+import random
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -9,5 +11,23 @@ if __name__ == '__main__':
     engine = create_engine('sqlite:///restaurbase.db')
     Session = sessionmaker(bind=engine)
     session = Session()
+    
+    session.query(Restaurant).delete()
+    session.query(Review).delete()
+    session.query(Customer).delete()
+    
+    fake = Faker()
+    
+    names = ['Kenmpinski', 'Sarova', 'Intercontinental', 'Sarova', 
+             'Serena', 'The One', 'Park View', 'Ole Sereni', 'Weston', 
+             'Radisson Blue','Hemingways', 'Sankara', 'The Tribe', 'The Boma' ]
+    
+    
+    restaurants = []
+    for item in range (50):
+        restaurant = Restaurant(
+            name = random.choice(names),
+            price =
+        )
 
     
