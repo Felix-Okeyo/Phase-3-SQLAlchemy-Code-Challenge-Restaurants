ee51c2f4d7c7987e38644488bb7d28c4556e062f
Felix-Okeyo <felixokeyo20@gmail.com>
2023-09-03 12:00:48 +0300

    migrations complete without methods

--- lib/models.py
+++ lib/models.py
@@ -7,7 +7,13 @@ engine = create_engine('sqlite:///restaurbase.db')
 
 Base = declarative_base()
 
-
+restaurant_customer = Table(
+    'restaurants_customers',
+    Base.metadata,
+    Column('restaurant_id', ForeignKey('restaurants.id'), primary_key = True),
+    Column('cusromer_id', ForeignKey('customers.id'), primary_key = True),
+    extend_existing = True,
+)
 
 class Restaurant(Base):
     __tablename__ = 'restaurants'
@@ -15,6 +21,15 @@ class Restaurant(Base):
     id = Column(Integer(), primary_key = True)
     name = Column(String())
     price = Column(Integer())
+    
+    customers = relationship('User', secondary = restaurant_customer, back_populates = 'restaurants')
+    reviews = relationship ('Review', backref = backref('restaurant'), cascade = 'all, delete-orphan')
+    
+    def __repr__(self):
+        return f'Restaurant (id={self.id}, ' + \
+            f'name = {self.name}, ' + \
+            f'price = {self.price})'
+
     
 
 class Customer(Base):
@@ -30,3 +45,10 @@ class Review(Base):
     id = Column(Integer(), primary_key = True)
     star_rating = Column(Integer())
     
+    restaurant_id = Column(Integer(), ForeignKey('restaurants.id'))
+    customer_id = Column(Integer(), ForeignKey('customers.id'))
+    
+    def __repr__(self):
+        return f'Review (id={self.id}, ' +\
+            f'star rating given = {self.star_rating}, ' +\
+            f'restaurant id ={self.restaurant_id})'
