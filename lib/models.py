bff53bd6a0f55a438fe22cfbf8ec89f1480c9295
Felix-Okeyo <felixokeyo20@gmail.com>
2023-09-05 11:34:53 +0300

    final commit

--- lib/models.py
+++ lib/models.py
@@ -1,89 +1,95 @@
+# Import necessary SQLAlchemy modules
 from sqlalchemy import create_engine, inspect
 from sqlalchemy import ForeignKey, Table, Column, Integer, String
 from sqlalchemy.orm import relationship, backref, session
 from sqlalchemy.ext.declarative import declarative_base
 
+# Create a SQLAlchemy database engine
 engine = create_engine('sqlite:///restaurbase.db')
 inspector = inspect(engine)
 
+# Create a base class for declarative models
 Base = declarative_base()
 
+# Define a many-to-many relationship table between restaurants and customers
 restaurant_customer = Table(
     'restaurants_customers',
     Base.metadata,
-    Column('restaurant_id', ForeignKey('restaurants.id'), primary_key = True),
-    Column('customer_id', ForeignKey('customers.id'), primary_key = True),
-    extend_existing = True,
+    Column('restaurant_id', ForeignKey('restaurants.id'), primary_key=True),
+    Column('customer_id', ForeignKey('customers.id'), primary_key=True),
+    extend_existing=True,
 )
 
+# Define the Restaurant class with attributes and relationships
 class Restaurant(Base):
     __tablename__ = 'restaurants'
     
-    id = Column(Integer(), primary_key = True)
+    id = Column(Integer(), primary_key=True)
     name = Column(String())
     price = Column(Integer())
     
-    customers = relationship('Customer', secondary = restaurant_customer, back_populates = 'restaurants')
-    reviews = relationship ('Review', backref = backref('restaurant'), cascade = 'all, delete-orphan')
+    customers = relationship('Customer', secondary=restaurant_customer, back_populates='restaurants')
+    reviews = relationship('Review', backref=backref('restaurant'), cascade='all, delete-orphan')
     
-    # return a collection of reviews associated with the restaurant 
+    # Return a collection of reviews associated with the restaurant 
     def restaurant_reviews(self):
         return self.reviews
     
-    #return a collection of all customers who reviewed the restaurant
+    # Return a collection of all customers who reviewed the restaurant
     def restaurant_customers(self):
         return self.customers 
     
-    #This method returns one restaurant instance with the highest price
+    # This method returns one restaurant instance with the highest price
     @classmethod
     def restaurant_fanciest(cls, session):
         return session.query(cls).order_by(cls.price.desc()).first()
     
-    #This should return an list of strings with all the reviews for this restaurant
-    #"Review for {insert restaurant name} by {insert customer's full name}: {insert review star_rating} stars.",
+    # This should return a list of strings with all the reviews for this restaurant
     def restaurant_all_reviews(self):
-        restaurant_review = session.query(Review).order_by(restaurant_id=self.id).all()
-        return[review.review_full_review() for review in restaurant_review]
+        restaurant_review = session.query(Review).filter_by(restaurant_id=self.id).all()
+        return [review.review_full_review() for review in restaurant_review]
 
-   
     def __repr__(self):
         return f'Restaurant (id={self.id}, ' + \
             f'name = {self.name}, ' + \
             f'price = {self.price})'
-            
+
+# Define the Customer class with attributes and relationships
 class Customer(Base):
     __tablename__ = 'customers'
     
-    id = Column(Integer(), primary_key = True)
+    id = Column(Integer(), primary_key=True)
     first_name = Column(String())
     last_name = Column(String())
     
-    restaurants = relationship('Restaurant', secondary = restaurant_customer, back_populates = 'customers')
-    reviews = relationship('Review', backref = backref('customer'), cascade = 'all, delete-orphan')
+    restaurants = relationship('Restaurant', secondary=restaurant_customer, back_populates='customers')
+    reviews = relationship('Review', backref=backref('customer'), cascade='all, delete-orphan')
     
-    #return a collection of reviews left by a customer 
+    # Return a collection of reviews left by a customer 
     def customer_reviews(self):
         return self.reviews 
-    #return a colletion of restaurants the customer has reviewed 
+    
+    # Return a collection of restaurants the customer has reviewed 
     def customer_restaurants(self):
         return self.restaurants 
     
-    # return customer full bame in a string that is concatenated
+    # Return customer full name in a string that is concatenated
     def full_name(self):
         return f'{self.first_name} {self.last_name}'
     
-   #should allow the customer to add a new review of a restaurant given a restaurant's id
+    # Allow the customer to add a new review of a restaurant given a restaurant's id
     def customer_add_review(self, restaurant, rating):
-        #create a new review by using the Review class attributes 
+        # Create a new review by using the Review class attributes 
         review = Review(
-        star_rating = rating, 
-        rastaurant_id = restaurant.id,
-        customer_id = self.id 
+            star_rating=rating, 
+            restaurant_id=restaurant.id,
+            customer_id=self.id 
         )
-        # append the review to the reviews list in the customer's reviews 
+        # Append the review to the reviews list in the customer's reviews 
         self.reviews.append(review)
     
-    def customer_delete_reviews(self,restaurant, session):
+    # Delete reviews for a specific restaurant
+    def customer_delete_reviews(self, restaurant, session):
         for review in self.reviews:
             if review.restaurant_id == restaurant.id:
                 session.delete(review) # Delete the review from the database
@@ -95,27 +101,28 @@ class Customer(Base):
         return f'Customer (id={self.id}),' +\
             f'name = {self.first_name},' +\
             f'price = {self.last_name})'
-    
+
+# Define the Review class with attributes and relationships
 class Review(Base):
     __tablename__ = 'reviews'
     
-    id = Column(Integer(), primary_key = True)
+    id = Column(Integer(), primary_key=True)
     star_rating = Column(Integer())
     
     restaurant_id = Column(Integer(), ForeignKey('restaurants.id'))
     customer_id = Column(Integer(), ForeignKey('customers.id'))
     
-    # restaurant = relationship('Restaurant', back_populates = 'reviews')
-    # customer = relationship('Customer', back_populates ='reviews')
-    # return the customer instance associated with the review 
-    # @property
+    # Return the customer instance associated with the review 
+    @property
     def review_customer(self):
         return self.customer_id 
-    # return restaurant instance associated with the review
-    # @property 
+    
+    # Return restaurant instance associated with the review
+    @property 
     def review_restaurant(self):
         return self.restaurant_id
         
+    # Return a string with the full review information
     def review_full_review(self):
         return f'Review for {self.review_restaurant().name} by {self.review_customer().full_name()}: {self.star_rating} stars'    
     
@@ -124,8 +131,5 @@ class Review(Base):
             f'star rating given = {self.star_rating}, ' +\
             f'restaurant id ={self.restaurant_id})'
 
+# Create the database tables based on the defined models
 Base.metadata.create_all(engine)
-
-
-# customer = session.query(Customer).filter_by(id=1).first()
-
--- lib/seed.py
+++ lib/seed.py
@@ -1,36 +1,35 @@
-
+# Import necessary modules
 from faker import Faker
 import random
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
-from models import Restaurant, Review, Customer
+from models import Restaurant, Review, Customer  # Import your SQLAlchemy models
 
 if __name__ == '__main__':
-    
+    # Create a SQLAlchemy database engine
     engine = create_engine('sqlite:///restaurbase.db')
     Session = sessionmaker(bind=engine)
     session = Session()
     
+    # Uncomment and use these lines to clear existing data in the database
     # session.query(Restaurant).delete()
     # session.query(Review).delete()
     # session.query(Customer).delete()
     
     fake = Faker()
     
-    names = ['Kenmpinski', 'Sarova', 'Intercontinental', 'Sarova', 
+    names = ['Kempinski', 'Sarova', 'Intercontinental', 'Sarova', 
              'Serena', 'The One', 'Park View', 'Ole Sereni', 'Weston', 
-             'Radisson Blue','Hemingways', 'Sankara', 'The Tribe', 'The Boma' ]
-    
+             'Radisson Blue', 'Hemingways', 'Sankara', 'The Tribe', 'The Boma']
     
     restaurants = []
-    for item in range (50):
+    for item in range(50):
         restaurant = Restaurant(
-            name = random.choice(names),
-            price = random.randint(200, 500)
+            name=random.choice(names),
+            price=random.randint(200, 500)
         )
         
-        # add and commit individually to get IDs back
+        # Add and commit individually to get IDs back
         session.add(restaurant)
         session.commit()
         
@@ -39,18 +38,17 @@ if __name__ == '__main__':
     customers = []
     for i in range(50):
         customer = Customer(
-            first_name = fake.first_name(),
-            last_name = fake.last_name(),
+            first_name=fake.first_name(),
+            last_name=fake.last_name(),
         )
         session.add(customer)
         session.commit()
         
         customers.append(customer)
         
-        
     reviews = []
     for restaurant in restaurants:
-        for item in range(random.randint(1,5)):
+        for item in range(random.randint(1, 5)):
             customer = random.choice(customers)
             if restaurant not in customer.restaurants:
                 customer.restaurants.append(restaurant)
@@ -58,15 +56,13 @@ if __name__ == '__main__':
                 session.commit()
             
             review = Review(
-                star_rating = random.randint(0,10),
-                restaurant_id = restaurant.id,
-                customer_id = customer.id,
+                star_rating=random.randint(0, 10),
+                restaurant_id=restaurant.id,
+                customer_id=customer.id,
             )
             reviews.append(review)
     
-    
+    # Use bulk_save_objects to efficiently insert reviews into the database
     session.bulk_save_objects(reviews)
     session.commit()
     session.close()
-
-    
